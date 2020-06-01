820018f702d2b8b9e0c4df3a577ea027ac6338ea
Rahul Chakrabarty <rahulchrty@gmail.com>
2020-06-02 00:59:34 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Catalogue} from './Catalogue';
+import {Layout} from './components/Layout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Layout>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Catalogue}/>
+        </Switch>
+      </Router>
+      </Layout>
+    </React.Fragment>
   );
 }
-
 export default App;
