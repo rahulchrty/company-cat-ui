090be8c078566e5864dacddf1a3d113f4858fc7a
Rahul Chakrabarty <rahulchrty@gmail.com>
2020-06-02 10:06:23 +0530

    make basic ajax

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Catalogue} from './Catalogue';
 import {Layout} from './components/Layout'
 
-function App() {
-  return (
-    <React.Fragment>
-      <Layout>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Catalogue}/>
-        </Switch>
-      </Router>
-      </Layout>
-    </React.Fragment>
-  );
+class App extends Component {
+  render() {
+      return (
+        <React.Fragment>
+        <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Catalogue}/>
+          </Switch>
+        </Router>
+        </Layout>
+      </React.Fragment>
+      )
+  }
+
+  state = {
+      contacts: []
+  };
+
+  componentDidMount() {
+      fetch('http://localhost:50449/api/catalogue')
+          .then(res => res.json())
+          .then((data) => {
+              this.setState({ contacts: data })
+              console.log(data);
+          })
+          .catch(console.log)
+  }
 }
 export default App;
